4e29a08eb57cfc4be642e4b9ed1f3e8dfa76d41e
phs <jo273175093@qq.com>
2017-11-12 13:57:07 +0800

    update

--- 实现树行组件/main.js
+++ 实现树行组件/main.js
@@ -19,23 +19,51 @@ var node = [{
 
 window.onload = function () {
   let container = document.querySelector('.container')
-  function render (node) {
+  function render (node, parentNode) {
     node.forEach(function (el) {
       if (el.children) {
         let folder = document.createElement('div')
         let sign = document.createElement('i')
         let folderIcon = document.createElement('i')
         let title = document.createElement('div')
-        sign.className = 'iconfont icon-jiajianzujianjiahao'
-        folderIcon.className = 'iconfont icon-wenjianjia1'
+        let children = document.createElement('div')
+        let folderOpen = false
+        folder.className = 'folder'
+        sign.className = 'iconfont icon-jiajianzujianjiahao left'
+        folderIcon.className = 'iconfont icon-wenjianjia1 left'
+        children.className = 'children'
         title.textContent = el.name
         title.appendChild(sign)
         title.appendChild(folderIcon)
         folder.appendChild(title)
+        render(el.children, children)
+        folder.appendChild(children)
+        title.addEventListener('click', function (e) {
+          if (folderOpen) {
+            sign.classList.remove('icon-jianhaojiacu')
+            sign.classList.add('icon-jiajianzujianjiahao')
+            folderIcon.classList.remove('icon-wenjianjia')
+            folderIcon.classList.add('icon-wenjianjia1')
+            children.style.display = 'none'
+          } else {
+            sign.classList.remove('icon-jiajianzujianjiahao')
+            sign.classList.add('icon-jianhaojiacu')
+            folderIcon.classList.add('icon-wenjianjia')
+            folderIcon.classList.remove('icon-wenjianjia1')
+            children.style.display = 'block'
+          }
+          folderOpen = !folderOpen
+        }, false)
+        parentNode.appendChild(folder)
       } else {
         let file = document.createElement('div')
-        let file
+        file.textContent = el.name
+        let fileIcon = document.createElement('i')
+        fileIcon.className = 'iconfont icon-wenjian left'
+        file.appendChild(fileIcon)
+        parentNode.appendChild(file)
       }
     })
   }
+  render(node, container)
 }
