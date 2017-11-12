63ef1a7cf4d79f3c041c2b2d2196c21848cb338c
phs <jo273175093@qq.com>
2017-11-12 14:14:22 +0800

    update

--- 实现树行组件/main.js
+++ 实现树行组件/main.js
@@ -1,32 +1,47 @@
-var node = [{
-  name: "父节点1",
-  children: [{
-    name: "子节点1"
-  }, {
-    name: "子节点2"
-  }]
-}, {
-  name: "父节点2",
-  children: [{
-    name: "子节点3"
-  }, {
-    name: "子节点4",
-    children: [{
-      name: "子节点5"
-    }]
-  }]
-}]
+var node = [
+  {
+    name: "style",
+    children: [
+      {
+      name: "reset.css"
+      },
+      {
+      name: "style.css"
+      }
+    ]
+  },
+  {
+    name: "js",
+    children: [
+      {
+        name: "main.js"
+      },
+      {
+        name: "util",
+        children: [
+          {
+          name: "dom.js"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'index.html'
+  }
+]
 
+// 暂时只完成了基本功能代码，后期再重构，代码写的比较乱
 window.onload = function () {
   let container = document.querySelector('.container')
   function render (node, parentNode) {
     node.forEach(function (el) {
       if (el.children) {
-        let folder = document.createElement('div')
-        let sign = document.createElement('i')
+        let folder = document.createElement('div') // 文件夹
+        let sign = document.createElement('i') // 加减号
         let folderIcon = document.createElement('i')
         let title = document.createElement('div')
-        let children = document.createElement('div')
+        let children = document.createElement('div') // 文件夹里面的内容区域
         let folderOpen = false
         folder.className = 'folder'
         sign.className = 'iconfont icon-jiajianzujianjiahao left'
@@ -60,6 +75,7 @@ window.onload = function () {
         file.textContent = el.name
         let fileIcon = document.createElement('i')
         fileIcon.className = 'iconfont icon-wenjian left'
+        file.className = 'file'
         file.appendChild(fileIcon)
         parentNode.appendChild(file)
       }
